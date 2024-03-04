316673a75b46cf7d71723bb45a34527d58cc6c97
erfan <erfan.moqadasii@gmail.com>
2024-03-04 14:06:10 +0330

    fix(form): delete function overloading on selector

--- packages/components/form/src/lib/utils/selector/selector.tsx
+++ packages/components/form/src/lib/utils/selector/selector.tsx
@@ -1,20 +1,15 @@
 import React, { Suspense, lazy } from 'react';
 import { FIELD_TYPE } from '../../../types/selector.types';
-import { TextFieldProps } from '@mui/material';
 
 export interface SelectorProps {
   fieldType: FIELD_TYPE;
   fieldProps: object;
 }
-export interface ITextInputProps {
-  fieldProps: TextFieldProps;
-}
 
-function Selector({ fieldType }: SelectorProps): React.ReactElement;
 function Selector({
   fieldType,
   fieldProps,
-}: SelectorProps & ITextInputProps): React.ReactElement {
+}: SelectorProps): React.ReactElement {
   let SelectedComponent;
 
   switch (fieldType) {
