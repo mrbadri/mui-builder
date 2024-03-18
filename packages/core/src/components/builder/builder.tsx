0e40b2bede6e938452e0abbf238295626c1d683c
MRBadri <badri0000badri@gmail.com>
2024-03-18 16:51:13 +0330

    enhance: core type and delete extra lines

--- packages/core/src/components/builder/builder.tsx
+++ packages/core/src/components/builder/builder.tsx
@@ -2,19 +2,13 @@ import React from 'react';
 import Selector from '../../utils/selector/selector';
 import { BuilderProps } from './builder.types';
 
-
 export const Builder: React.FC<BuilderProps> = ({ groupList }) => {
-
-  return (
-    <>
-      {groupList.map((data, index) => (
-        <Selector
-          key={`${data.id}-index-${index}`}
-          groupType={data.groupType}
-          fieldType={data.type}
-          fieldProps={data.props}
-        />
-      ))}
-    </>
-  );
+  return groupList.map((data, index) => (
+    <Selector
+      key={`${data.id}-index-${index}`}
+      groupType={data.groupType}
+      fieldType={data.type}
+      fieldProps={data.props}
+    />
+  ));
 };
