40d645311d8712ec4bab28b5aa7e7a4661e53352
erfan <erfan.moqadasii@gmail.com>
2024-03-25 16:15:10 +0330

    feat(form): api builder types

--- packages/components/form/src/utils/api/builder/apiBuilder.ts
+++ packages/components/form/src/utils/api/builder/apiBuilder.ts
@@ -1,28 +1,43 @@
 import { convertFn, isStrFn } from '@mui-builder/utils';
-import { ApiBuilderProps } from './apiBuilder.types';
+import {
+  ApiBuilderProps,
+  dataBuilderFn,
+  headersBuilderFn,
+  paramsBuilderFn,
+  urlBuilderFn,
+} from './apiBuilder.types';
 
-const apiBuilder = async ({ apiInstance, apiConfigs }: ApiBuilderProps) => {
+const apiBuilder = async ({
+  apiInstance,
+  apiConfigs,
+  formMethods,
+}: ApiBuilderProps) => {
   const { url, data, params, headers, ...configs } = apiConfigs;
 
   const urlBuilder = () => {
     if (isStrFn(url))
-      return convertFn<(url: string) => string>(url, 'test')('test');
+      return convertFn<urlBuilderFn>(url, 'formMethods')(formMethods);
     return url;
   };
 
   const dataBuilder = () => {
     if (isStrFn(data))
-      return convertFn<(data: unknown) => any>(data, 'test')('test');
+      return convertFn<dataBuilderFn>(data, 'formMethods')(formMethods);
     return data;
   };
 
   const paramsBuilder = () => {
-    if (isStrFn(params)) return convertFn<any>(params, 'test')('test');
+    if (isStrFn(params))
+      return convertFn<paramsBuilderFn>(params, 'formMethods')(formMethods);
     return params;
   };
 
   const headersBuilder = () => {
-    if (isStrFn(headers)) return convertFn<any>(headers as any, 'test')('test');
+    if (isStrFn(headers))
+      return convertFn<headersBuilderFn>(
+        JSON.stringify(headers),
+        'formMethods'
+      )(formMethods);
     return headers;
   };
 
--- packages/components/form/src/utils/api/builder/apiBuilder.types.ts
+++ packages/components/form/src/utils/api/builder/apiBuilder.types.ts
@@ -1,4 +1,5 @@
-import { AxiosRequestConfig, AxiosStatic } from 'axios';
+import { AxiosHeaders, AxiosRequestConfig, AxiosStatic } from 'axios';
+import { Form } from '../../../hooks/useForms/useForms';
 
 export type ApiInstance = AxiosStatic;
 
@@ -7,4 +8,13 @@ export type ApiConfigs = AxiosRequestConfig;
 export type ApiBuilderProps = {
   apiInstance: ApiInstance;
   apiConfigs: ApiConfigs;
+  formMethods: Form;
 };
+
+export type urlBuilderFn = (formMethods: Form) => string;
+
+export type dataBuilderFn = (formMethods: Form) => unknown;
+
+export type paramsBuilderFn = (formMethods: Form) => unknown;
+
+export type headersBuilderFn = (formMethods: Form) => AxiosHeaders;
