445a567d5bca1654ba1bbf3632d30277e84def32
MRBadri <badri0000badri@gmail.com>
2024-04-16 14:06:18 +0330

    feat: handle single children and array children in grid

--- packages/components/grid/src/components/grid/grid.type.ts
+++ packages/components/grid/src/components/grid/grid.type.ts
@@ -3,5 +3,5 @@ import { Grid2Props } from '@mui/material';
 import { FormBuilderProps } from '../coreBuilder/builder.types';
 
 export type GridProps = Omit<Grid2Props, 'children'> & {
-  children?: FormBuilderProps;
+  children?: FormBuilderProps | FormBuilderProps[];
 };
